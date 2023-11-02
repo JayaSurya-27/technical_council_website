b2739658d5bf588ca171ecd174ab2aa847836b9d
Utkarsh1343 <ahmednagaruser414006@gmail.com>
2023-11-02 22:57:23 +0530

    Header,Footer and Favicon added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavBar from "./components/navBar";
 import Home from "./pages/home";
 import Events from "./pages/events";
 import Members from "./pages/members";
 import ApiState from "./context/apiState";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
       <Router>
         <ApiState>
-          {/* <Navbar /> */}
-
-          <Routes>
+          <NavBar/>
+          {/* <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/events" element={<Events />} />
             <Route path="/Members" element={<Members />} />
-          </Routes>
+            </Routes>*/}
+            <Footer />
         </ApiState>
       </Router>
     </>
--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,10 +1,87 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import styles from './navBar.module.css'
+import image from './TechC_Logo.jpg'
 
 function NavBar() {
+    const [active, setActive] = useState(false)
+    const clickHandler = () => {
+        setActive(prevActive => !prevActive)
+      }
+    
+      const [visible, setVisible] = useState(true)
+      const [previousScrollPosition, setPreviousScrollPosition] = useState(0)
+      useEffect(() => {
+        const handleScroll = () => {
+          const currentScrollPos = window.pageYOffset
+          const visible = previousScrollPosition > currentScrollPos
+    
+          setPreviousScrollPosition(currentScrollPos)
+          setVisible(visible)
+        }
+        window.addEventListener('scroll', handleScroll)
+        return () => window.removeEventListener('scroll', handleScroll)
+      }, [previousScrollPosition])
     return (
-        <nav>
-            
+        <div id='homeId'>
+      <nav
+        className={styles.navBarWrapper}
+        style={{ top: visible ? 0 : '-6rem' }}
+      >
+        <div className={styles.navBarMain}>
+          <div className={styles.logoWrapper} style={{display:'contents'}}>
+          <a href={`/`}>
+              <img
+                src={image}
+                width={70}
+                height={50}
+                alt='Picture of the Council'
+                priority
+              />
+            </a>
+            <div className={styles.optionsWrapper}>
+            <div
+              className={`${styles.optionsMenu} ${
+                active && styles.activeSidebar
+              } font-sans`}
+            ></div>
+            <div className={styles.option}>
+            <a href={`/`} scroll={false}>
+                  <div>Clubs</div>
+                </a>
+            </div>
+            <div className={styles.option}>
+                <a href={`/`} scroll={false}>
+                  <div>About Us</div>
+                </a>
+              </div>
+              <div className={styles.option}>
+                <a href={`/`} scroll={false}>
+                  <div>Members</div>
+                </a>
+              </div>
+              <div className={styles.option}>
+                <a href={`/`} scroll={false}>
+                  <div>Events</div>
+                </a>
+              </div>
+            </div>
+            <div className={styles.hamBurgerWrapper}>
+              <div
+                className={`${styles.hamburger} ${
+                  active ? styles.active : styles['not-active']
+                }`}
+                onClick={clickHandler}
+              >
+                <span className={styles.spans}></span>
+                <span className={styles.spans}></span>
+                <span className={styles.spans}></span>
+              </div>
+            </div>
+          </div>
+          </div>
         </nav>
+        </div>
     );
 }
 
