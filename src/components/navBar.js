67ad47531ef2c4ed9b01dcd11b7d21fc1aebbac5
Utkarsh1343 <ahmednagaruser414006@gmail.com>
2023-11-05 16:42:46 +0530

    Spacing and Hamburger errors fixed

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,69 +1,68 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
 import styles from './navBar.module.css'
-import image from './TechC_Logo.jpg'
+import image_src from "./TechC_Logo.jpg"
 
-function NavBar() {
-    const [active, setActive] = useState(false)
-    const clickHandler = () => {
-        setActive(prevActive => !prevActive)
-      };
-      const [visible, setVisible] = useState(true);
-      const [previousScrollPosition, setPreviousScrollPosition] = useState(0);
-      useEffect( function ()  {
-        const handleScroll = function () {
-          const currentScrollPos = window.scrollY;
-          const visible = previousScrollPosition > currentScrollPos
-    
-          setPreviousScrollPosition(currentScrollPos);
-          setVisible(visible);
-        };
-        window.addEventListener('scroll', handleScroll);
-        return function () { 
-          window.removeEventListener('scroll', handleScroll);
-        };
-      }, [previousScrollPosition]);
-      
-    return (
-        <div id='homeId'>
+export default function AppBar() {
+  const [active, setActive] = useState(false)
+  const clickHandler = () => {
+    setActive(prevActive => !prevActive)
+  }
+
+  const [visible, setVisible] = useState(true)
+  const [previousScrollPosition, setPreviousScrollPosition] = useState(0)
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY
+      const visible = previousScrollPosition > currentScrollPos
+
+      setPreviousScrollPosition(currentScrollPos)
+      setVisible(visible)
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [previousScrollPosition])
+
+  return (
+    <div id='homeId'>
       <nav
         className={styles.navBarWrapper}
         style={{ top: visible ? 0 : '-6rem' }}
       >
         <div className={styles.navBarMain}>
-          <div className={styles.logoWrapper} style={{display:'contents'}}>
-          <a href={`/`}>
+          <div className={styles.logoWrapper}>
+            <a href={`/`}>
               <img
-                src={image}
+                src={image_src}
                 width={70}
                 height={50}
-                alt='Picture of the Council'
-                priority="true"
+                alt='ClubPic'
+                priority
               />
             </a>
-            <div className={styles.optionsWrapper}>
+          </div>
+          <div className={styles.optionsWrapper}>
             <div
               className={`${styles.optionsMenu} ${
                 active && styles.activeSidebar
               } font-sans`}
-            ></div>
-            <div className={styles.option}>
-            <a href={`/`} scroll="false">
+            >
+              <div className={styles.option}>
+                <a href={`/`} scroll={false}>
                   <div>Clubs</div>
                 </a>
-            </div>
-            <div className={styles.option}>
-                <a href={`/`} scroll="false">
+              </div>
+              <div className={styles.option}>
+                <a href={`/`} scroll={false}>
                   <div>About Us</div>
                 </a>
               </div>
               <div className={styles.option}>
-                <a href={`/`} scroll="false">
+                <a href={`/`} scroll={false}>
                   <div>Members</div>
                 </a>
               </div>
               <div className={styles.option}>
-                <a href={`/`} scroll="false">
+                <a href={`/`} scroll={false}>
                   <div>Events</div>
                 </a>
               </div>
@@ -81,10 +80,8 @@ function NavBar() {
               </div>
             </div>
           </div>
-          </div>
-        </nav>
         </div>
-    );
+      </nav>
+    </div>
+  )
 }
-
-export default NavBar;
